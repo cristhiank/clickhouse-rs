4588861db262092af7be7bcdb781012fdfc09a43
Nick Blakely <niblak@microsoft.com>
2025-02-11 15:06:11 -0800

    Fix setting query ID from the client.

--- src/types/cmd.rs
+++ src/types/cmd.rs
@@ -81,7 +81,8 @@ fn encode_query(query: &Query, context: &Context) -> Result<Vec<u8>> {
     // DBMS_MIN_REVISION_WITH_CLIENT_INFO
     let mut encoder = Encoder::new();
     encoder.uvarint(protocol::CLIENT_QUERY);
-    encoder.string("");
+
+    encoder.string(query.get_id());
 
     {
         let hostname = &context.hostname;
